c726591f9dac2305ad1171078c78b367f49ce3b9
AlicePlentz <alicecplentz@gmail.com>
2024-02-12 17:18:01 -0300

    Web 1 v2 - funções

--- Program.cs
+++ Program.cs
@@ -4,32 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Infome a primeira nota: ");
-        double n1 = Convert.ToDouble(Console.ReadLine());
+        
+        float n1 = PedirNota("Informe a primeira nota: ");
+        float n2 = PedirNota("Informe a segunda nota: ");
+        float n3 = PedirNota("Informe a terceira nota: ");
 
-        Console.WriteLine("Segunda nota: ");
-        double n2 = Convert.ToDouble(Console.ReadLine());
+        float media = CalcularMedia(n1, n2, n3);
 
-        Console.WriteLine("Terceira nota: ");
-        double n3 = Convert.ToDouble(Console.ReadLine());
+        string sit = Situacao(media);
+       
+        Escrever($"Sua média é {media,0:N1}");
+        Escrever($"Situação: {sit}");
 
-        double media = (n1 + n2 + n3) / 3;
+    }
 
+    public static void Escrever(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
+    public static float PedirNota(string msg) 
+    {
+       Console.WriteLine(msg);
+       float n = Convert.ToSingle(Console.ReadLine());
+
+       return n;
+    }
+
+
+    public static float CalcularMedia(float n1, float n2, float n3)
+    {
+        float m = (n1 + n2 + n3) / 3;
+
+        return m;
+    }
+
+
+    public static string Situacao(float media)
+    {
         string sit = "";
-        if (media >= 6) 
+        if (media >= 6)
         {
-            sit = "Aprovado!";
-        } 
-        else 
+            sit = "Aprovado!"; 
+        }
+        else
         {
             sit = "Reprovado";
         }
 
-        Console.WriteLine($"Sua média é {media}");
-        Console.WriteLine($"Situação: {sit}");
-
+        return sit;
     }
-
-
     
 }
+       
+
+
