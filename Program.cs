5f5ea0267c157abc386fa30df25a8bd5f57fc99d
AlicePlentz <alicecplentz@gmail.com>
2024-02-13 10:48:57 -0300

    web 1 v3 - recursiva

--- Program.cs
+++ Program.cs
@@ -4,58 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        float n1 = PedirNota("Informe a primeira nota: ");
-        float n2 = PedirNota("Informe a segunda nota: ");
-        float n3 = PedirNota("Informe a terceira nota: ");
+        //fatorial de 5 = 5 * 4 * 3 * 2 * 1
 
-        float media = CalcularMedia(n1, n2, n3);
-
-        string sit = Situacao(media);
-       
-        Escrever($"Sua média é {media,0:N1}");
-        Escrever($"Situação: {sit}");
-
+        int fat = Fatorial(5);
+        Console.WriteLine(fat);
     }
 
-    public static void Escrever(string msg)
+    public static int Fatorial(int n)
     {
-        Console.WriteLine(msg);
-    }
-
-    public static float PedirNota(string msg) 
-    {
-       Console.WriteLine(msg);
-       float n = Convert.ToSingle(Console.ReadLine());
-
-       return n;
-    }
-
-
-    public static float CalcularMedia(float n1, float n2, float n3)
-    {
-        float m = (n1 + n2 + n3) / 3;
-
-        return m;
-    }
-
-
-    public static string Situacao(float media)
-    {
-        string sit = "";
-        if (media >= 6)
+        if (n == 1)
         {
-            sit = "Aprovado!"; 
+            return 1;
         }
-        else
-        {
-            sit = "Reprovado";
-        }
-
-        return sit;
+        return n * Fatorial(n-1);
     }
     
 }
-       
-
-
